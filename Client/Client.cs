e7f5c300c0f52e0c082c0069177e76684b6366f7
Костя Жихарев <kostyazhikharev@gmail.com>
2020-11-15 14:14:30 +0300

    add client class

---
+++ Client/Client.cs
@@ -0,0 +1,32 @@
+
+using System;
+using System.Net.Sockets;
+
+namespace ClientClassNamespace
+{
+    public class ClientClass
+    {
+
+        private readonly string _serverAddress;
+        private readonly int _port;
+        private NetworkStream _stream;
+
+        public ClientClass(string serverAdress,int port)
+        {
+            _serverAddress = serverAdress;
+            _port = port;
+        }
+        public void Connect()
+        { 
+              
+                TcpClient client = new TcpClient(_serverAddress, _port);
+                _stream = client.GetStream();
+        }
+
+        public void SendMessage(string message)
+        {
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                _stream.Write(data, 0, data.Length);
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Sockets;
+using ClientClassNamespace;
 
 namespace Client
 {
@@ -8,6 +9,10 @@ namespace Client
         static void Main(string[] args)
         {
             Connect("127.0.0.1", "String message");
+
+            ClientClass client = new ClientClass("127.0.0.1",13000);
+            client.Connect(); 
+            client.SendMessage("Message to server");
         }
         //Functions:
         //Connect()
